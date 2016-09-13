b9f30de1c2ad0a068db6fd88cd1161fd522600bd
Maryna Leanovich <sollferino@gmail.com>
2016-09-13 20:08:19 +0300

    .-.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+  window.onscroll = function up() {
+      document.getElementById('up-button').style.display = (window.pageYOffset > '300' ? 'block' : 'none');
+  }
+
+  $('a[href^="#"]').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+    return false;
+  });
+
+$('.up').click(function(){
+    $('html, body').animate({scrollTop: '0px'}, 800);
+      return false;
+     });
+
+  $('#feed-slide').dbpasCarousel({
+      itemsVisible: 1
+  });
+
+
 });
