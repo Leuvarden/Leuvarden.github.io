3f647f7a6a3486a8ce886c6c2df082e1239001b3
Maryna Leanovich <sollferino@gmail.com>
2019-11-28 12:56:47 +0300

    a11y training q2

---
+++ a11y-training-q2/js/custom.js
@@ -0,0 +1,58 @@
+(function() {
+  var burger = document.querySelector(".burger");
+  var menu = document.querySelector("#" + burger.dataset.target);
+  burger.addEventListener("click", function() {
+    burger.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
+  });
+})();
+
+var adminMenu = document.getElementById('admin_menu');
+adminMenu.addEventListener("click", function() {
+  adminMenu.classList.toggle("is-active");
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
+
+document.querySelectorAll("#nav li").forEach(function(navEl) {
+  navEl.onclick = function() {
+    toggleTab(this.id, this.dataset.target);
+  };
+});
+
+function toggleTab(selectedNav, targetId) {
+  var navEls = document.querySelectorAll("#nav li");
+
+  navEls.forEach(function(navEl) {
+    if (navEl.id == selectedNav) {
+      navEl.classList.add("is-active");
+    } else {
+      if (navEl.classList.contains("is-active")) {
+        navEl.classList.remove("is-active");
+      }
+    }
+  });
+
+  var tabs = document.querySelectorAll(".tab-pane");
+
+  tabs.forEach(function(tab) {
+    if (tab.id == targetId) {
+      tab.style.display = "block";
+    } else {
+      tab.style.display = "none";
+    }
+  });
+}
