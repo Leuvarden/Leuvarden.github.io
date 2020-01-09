8f07a4045d0fa3318e3ed807069b866ffe94e384
Maryna Leanovich <sollferino@gmail.com>
2020-01-09 18:27:09 +0300

    another homework

--- rs-homework/js/custom.js
+++ rs-homework/js/custom.js
@@ -7,32 +7,70 @@
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
+document.querySelectorAll('#nav .tab-control').forEach(function(navEl, index, navElArray) {
   navEl.onclick = function() {
     toggleTab(this.id, this.dataset.target);
   };
+
+  navEl.onkeydown = function(event) {
+    if (['ArrowLeft', 'Left', 'ArrowRight', 'Right', 'Home', 'End', 'Space', 'Enter'].includes(event.key)) {
+      event.preventDefault();
+
+      let currentActiveElement = navElArray[index];
+      switch (event.code) {
+        case 'Space':
+        case 'Enter':
+          toggleTab(currentActiveElement.id, currentActiveElement.dataset.target);
+          break;
+        case 'ArrowRight':
+          currentActiveElement = index + 1 === navElArray.length ? navElArray[0] : navElArray[index + 1];
+          break;
+        case 'ArrowLeft':
+          currentActiveElement = index - 1 === -1 ? navElArray[navElArray.length - 1] : navElArray[index - 1];
+          break;
+        case 'Home':
+          currentActiveElement = navElArray[0];
+          break;
+        case 'End':
+          currentActiveElement = navElArray[navElArray.length - 1];
+          break;
+        default:
+          break;
+      }
+
+      currentActiveElement.focus();
+
+    }
+  }
 });
 
 function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
+  var navEls = document.querySelectorAll('#nav .tab-control');
 
   navEls.forEach(function(navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
+    if (navEl.id === selectedNav) {
+      navEl.classList.add('is-active');
+      navEl.setAttribute('aria-selected', 'true');
+      navEl.removeAttribute('tabindex');
     } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
+      if (navEl.classList.contains('is-active')) {
+        navEl.classList.remove('is-active');
       }
+      navEl.setAttribute('aria-selected', 'false');
+      navEl.setAttribute('tabindex', '-1')
     }
   });
 
-  var tabs = document.querySelectorAll(".tab-pane");
+  var tabsPanels = document.querySelectorAll('.tab-pane');
 
-  tabs.forEach(function(tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
+  tabsPanels.forEach(function(tabPanel) {
+    if (tabPanel.id === targetId) {
+      tabPanel.style.display = 'block';
+      tabPanel.setAttribute('aria-expanded', 'true');
     } else {
-      tab.style.display = "none";
+      tabPanel.style.display = 'none';
+      tabPanel.setAttribute('aria-expanded', 'false');
     }
   });
 }
+
